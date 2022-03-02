43d775e06b8438255a55e79a361f2299abb2e937
kvello <markus.kv1@gmail.com>
2022-03-02 20:47:25 +0100

    Function definition and moved def

--- skeleton_project/source/utils.h
+++ skeleton_project/source/utils.h
@@ -1,4 +1,6 @@
 #pragma once
+#define NUM_STATE_VARIABLES 5
+#define NUM_ACTIONS 4
 
 /**
  * @brief Hjelpe struktur for sann/usann verdier
@@ -6,5 +8,10 @@
  */
 typedef enum Bool{f, t} Bool;
 
-void columnWiseAnd(Bool* data_vector, Bool table[][], Bool result_table[][]);
-void columnWiseComparison(Bool table1[][], Bool table2[][], Bool* result_column);
+
+
+
+
+
+void columnWiseAnd(Bool* data_vector, Bool table[][NUM_ACTIONS], Bool result_table[][NUM_ACTIONS]);
+void columnWiseComparison(Bool table1[][NUM_ACTIONS], Bool table2[][NUM_ACTIONS], Bool* result_column);
