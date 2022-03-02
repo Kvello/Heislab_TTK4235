233c21c9f23a52745ee82200dbe5ae001f5c242f
kvello <markus.kv1@gmail.com>
2022-03-02 20:48:18 +0100

    nextAction

--- skeleton_project/source/elevator.c
+++ skeleton_project/source/elevator.c
@@ -39,7 +39,7 @@ void elevatorInit(Elevator* elevator){
     elevator->door_open     = f;
     elevator->current_floor =undefined;
 }
-/*void nextAction(Elevator* elevator){
+void nextAction(Elevator* elevator){
     Floor current_order =   getNextOrder(&(elevator->order_system));
     Bool data_vector[NUM_STATE_VARIABLES] = {elevator->door_open,
                                             getElevatorFloor(elevator)>current_order,
@@ -55,7 +55,7 @@ void elevatorInit(Elevator* elevator){
             executeAction(i, elevator);
         }
     }
-}*/
+}
 
 void executeAction(int action_num, Elevator* elevator){
     if(action_num == 0){
