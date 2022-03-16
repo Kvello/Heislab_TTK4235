2e0a95e50d187dbb9559b14cb0c9f27718ab5147
kvello <markus.kv1@gmail.com>
2022-03-16 14:37:47 +0100

    updateFloor

--- skeleton_project/source/elevator.c
+++ skeleton_project/source/elevator.c
@@ -237,44 +237,6 @@ void executeRule(Rule rule, Elevator* elevator){
     }
 }
 
-void checkButtons(Elevator* elevator){
-    for(int f = 0; f < N_FLOORS; f++){
-        for(int b = 0; b < N_BUTTONS; b++){
-            int btnPressed = elevio_callButton(f, b);
-            if(btnPressed){
-                newOrder(&(elevator->order_system),f , b); // Updates orders when a button is pressed
-            }
-        }
-    }
-
-    if(elevio_obstruction()){
-        elevator->obstructed=t;
-    } else {
-        elevator->obstructed=f;
-    }
-        
-    if(elevio_stopButton()){
-        elevator->stop_btn = t;
-    }
-    else{
-        elevator->stop_btn = f;
-    }
-}
-
-void setLamps(Elevator* elevator){
-    if(elevator->current_floor != undefined){
-        elevio_floorIndicator(elevator->current_floor); // Lamp lights up when passing new floor
-    }
-    
-    elevio_stopLamp(elevator->stop_btn); // Stoplamp lights up when stop button is pressed
-    elevio_doorOpenLamp(elevator->door_open); // Door lamp lights up when door is open
-    
-    for(int f = 0; f < N_FLOORS; f++){
-        for(int b = 0; b < N_BUTTONS; b++){
-            elevio_buttonLamp(f, b, elevator->order_system.orders[f][b]); // Lights the lamp of the floors in orders
-        }
-    }   
-}
 
 void updateElevatorOrder(Elevator* elevator){
     updateNextOrder(&(elevator->order_system));
--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -3,7 +3,55 @@
 #include <signal.h>
 #include <time.h>
 #include "elevator.h"
+void checkButtons(Elevator* elevator){
+    for(int f = 0; f < N_FLOORS; f++){
+        for(int b = 0; b < N_BUTTONS; b++){
+            int btnPressed = elevio_callButton(f, b);
+            if(btnPressed){
+                newOrder(&(elevator->order_system),f , b); // Updates orders when a button is pressed
+            }
+        }
+    }
 
+    if(elevio_obstruction()){
+        elevator->obstructed=t;
+    } else {
+        elevator->obstructed=f;
+    }
+        
+    if(elevio_stopButton()){
+        elevator->stop_btn = t;
+    }
+    else{
+        elevator->stop_btn = f;
+    }
+}
+
+void setLamps(Elevator* elevator){
+    if(elevator->current_floor != undefined){
+        elevio_floorIndicator(elevator->current_floor); // Lamp lights up when passing new floor
+    }
+    
+    elevio_stopLamp(elevator->stop_btn); // Stoplamp lights up when stop button is pressed
+    elevio_doorOpenLamp(elevator->door_open); // Door lamp lights up when door is open
+    
+    for(int f = 0; f < N_FLOORS; f++){
+        for(int b = 0; b < N_BUTTONS; b++){
+            elevio_buttonLamp(f, b, elevator->order_system.orders[f][b]); // Lights the lamp of the floors in orders
+        }
+    }   
+}
+
+void updateFloor(Elevator* elevator){
+    int floor = elevio_floorSensor();
+    if(floor != undefined){
+        elevator->current_floor = floor;
+        elevator->between_floors = f;
+    }
+    else if(floor == undefined){
+        elevator->between_floors = t;
+    }
+}
 
 int main(){
     elevio_init();
@@ -12,20 +60,11 @@ int main(){
     printf("=== Elevator ===\n");
     elevatorInit(&elevator);
     while(1){
-        int floor = elevio_floorSensor();
-        if(floor != undefined){
-            elevator.current_floor = floor;
-            elevator.between_floors = f;
-        }
-        else if(floor == undefined){
-            elevator.between_floors = t;
-        }
-        
+        updateFloor(&elevator);
         checkButtons(&elevator);
         nextAction(&elevator);
         elevio_motorDirection(elevator.dir);
         setLamps(&elevator);
-        
         nanosleep(&(struct timespec){0, 20*1000*1000}, NULL);
     }
 
